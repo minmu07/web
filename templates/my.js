bd5ab64b1374fc0bc509736ca65e7cc17232ce5a
xiangzhou <1725526005@qq.com>
2025-02-27 20:34:45 +0800

    测试4

---
+++ templates/my.js
@@ -0,0 +1,7 @@
+// type="text/javascript" 用于指定 <script> 标签中的内容类型为 JavaScript。
+// 它的作用是告诉浏览器该脚本是 JavaScript 代码，以便正确解析和执行。
+// 在现代 HTML5 中，type="text/javascript" 可以省略，因为 JavaScript 是 <script> 标签的默认类型。
+function myFunc() {
+    //alert("你好呀！");
+    confirm("是否要继续？");
+}
